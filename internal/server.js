15b2d83f16eba9da85acb2faa287bf90a6f3cf7d
snehasaloni12 <snsaloni@deloitte.com>
2022-02-17 05:49:26 +0000

    adding chnages

--- internal/server.js
+++ internal/server.js
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
 // from a cloud data store
 const mockEvents = {
     events: [
-        { title: 'an event', id: 1, description: 'something really cool' },
+        { title: 'an event', id: 1, description: 'something really cool!' },
         { title: 'another event', id: 2, description: 'something even cooler' }
     ]
 };
